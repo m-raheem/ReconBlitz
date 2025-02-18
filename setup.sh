8a321c7b915f69a60f60a10ae27d9588cad6bda4
Muhammad Raheem Arif <raheemarif710@gmail.com>
2025-02-18 23:01:06 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
 
 echo "Setting up ReconBlitz..."
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
 echo "Updating package lists..."
 sudo apt update -y
 
-echo "Installing Go..."
-sudo apt install -y golang
-
-echo "Setting up Go environment..."
-export PATH=$PATH:/usr/local/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-source ~/.bashrc
+echo "Checking and Installing Go..."
+if ! command_exists go; then
+    sudo apt install -y golang
+    echo "Setting up Go environment..."
+    export PATH=$PATH:/usr/local/go/bin
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+    source ~/.bashrc
+else
+    echo "Go is already installed."
+fi
 
 echo "Installing required tools..."
-go install -v github.com/OWASP/Amass/v3/...@master
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install -v github.com/tomnomnom/assetfinder@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install -v github.com/sensepost/gowitness@latest
-go install -v github.com/haccer/subjack@latest
-go install -v github.com/lc/gau@latest
-go install -v github.com/hakluke/hakrawler@latest
-go install -v github.com/tomnomnom/waybackurls@latest
-go install -v github.com/projectdiscovery/katana/cmd/katana@latest
+declare -A tools=(
+    ["amass"]="github.com/OWASP/Amass/v3/...@master"
+    ["subfinder"]="github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
+    ["assetfinder"]="github.com/tomnomnom/assetfinder@latest"
+    ["httpx"]="github.com/projectdiscovery/httpx/cmd/httpx@latest"
+    ["gowitness"]="github.com/sensepost/gowitness@latest"
+    ["subjack"]="github.com/haccer/subjack@latest"
+    ["gau"]="github.com/lc/gau@latest"
+    ["hakrawler"]="github.com/hakluke/hakrawler@latest"
+    ["waybackurls"]="github.com/tomnomnom/waybackurls@latest"
+    ["katana"]="github.com/projectdiscovery/katana/cmd/katana@latest"
+)
+
+for tool in "${!tools[@]}"; do
+    if ! command_exists "$tool"; then
+        echo "Installing $tool..."
+        go install -v "${tools[$tool]}"
+    else
+        echo "$tool is already installed."
+    fi
+done
 
 echo "Moving script to /usr/local/bin/..."
 sudo mv reconblitz.sh /usr/local/bin/reconblitz
 sudo chmod +x /usr/local/bin/reconblitz
 
 echo "Installation completed!"
-echo "To start using ReconBlitz, run: ./reconblitz.sh <domain>"
+echo "To start using ReconBlitz, run: reconblitz <domain>"
